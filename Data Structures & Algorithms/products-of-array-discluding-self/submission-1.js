2950b2d0031665e152a2673ef2d07d17866fe371
Neeraj Kumar <50705472+neerajkumar161@users.noreply.github.com>
2026-05-04 23:20:36 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const prefixArr = [1];
+        const suffixArr = [1];
+
+        nums.forEach((el, idx) => {
+            prefixArr.push(prefixArr[idx] * el);
+        });
+        for (let i = nums.length - 1; i >= 0; i--) {
+            suffixArr.splice(0, 0, suffixArr[0] * nums[i]);
+        }
+
+        console.log(prefixArr, suffixArr);
+
+        const res = [];
+        prefixArr.forEach((el, idx) => {
+            if(res.length == nums.length) return;
+            res.push(prefixArr[idx] * suffixArr[idx + 1])
+        });
+
+        return res;
+    }
+}
