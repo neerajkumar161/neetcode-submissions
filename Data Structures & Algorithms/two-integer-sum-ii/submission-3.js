2073565bffa14cee248c6764febfb1a43a0d4cdd
Neeraj Kumar <50705472+neerajkumar161@users.noreply.github.com>
2026-05-08 00:00:43 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+
+        // Binary Search
+        let left = 0;
+        let right = numbers.length - 1;
+
+        while(left < right) {
+            const sums = numbers[left] + numbers[right];
+            if(sums == target) return [left + 1, right + 1];
+
+            if(sums > target) {
+                // find in left
+                right--;
+            }
+
+            if(sums < target) {
+                // find in right
+                left++;
+            }
+
+        }
+
+        return []
+    }
+}
